185ef78c5059554f5475c6a5d1f7b59dde399fb9
rabbitglauser <samuel.glauser@gmail.com>
2024-09-02 13:05:51 +0200

    Update method descriptions and improve constructor comments
    Detailed comments were added to methods for user input and calculations in `calculator.cs` to clarify their functionality. Additionally, comments were enhanced in the constructor to reflect the initialization of width and height properties.

--- learning/calculator.cs
+++ learning/calculator.cs
@@ -3,13 +3,19 @@
     private double _width = 22;
     private double _height =  9;
     
+    //defining the width and the height in my methode
     public Rectangle(double width, double height)
     {
         _width = width;
         _height = height;
         Console.WriteLine("Welcome to the Rectangle Calculator!");
     }
-    
+
+
+    /// Prompts the user to enter the width and height of the rectangle,
+    /// and sets the respective properties with the provided values.
+    /// Function will request user input and converts it to double values
+    /// to update the dimensions of the rectangle.
     public void GetDimensionsFromUser()
     {
         Console.Write("Please enter the width of the rectangle: ");
@@ -20,12 +26,16 @@
         _height = Convert.ToDouble(heightInput);
     }
 
+  
+    /// Calculates and prints the perimeter of the rectangle
+    /// using the width and height properties.
     public void CalculatePerimeter()
     {
         double perimeter = 2 * (_width + _height);
         Console.WriteLine($"The perimeter of the rectangle is: {perimeter}");
     }
     
+    /// Calculates the area of the rectangle using its width and height, and outputs the result to the console.
     public void CalculateArea()
     {
         double area = _width * _height;
